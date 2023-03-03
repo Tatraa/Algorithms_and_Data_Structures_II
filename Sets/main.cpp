ebff8aebd7a736f1bfd676c88247551f121e99ea
Tatraa <tatra5342@gmail.com>
2023-03-03 13:05:00 +0100

    Sets added

---
+++ Sets/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+struct Cos {
+
+};
+
+struct inneCos {
+
+};
+
+
+/*isMember(x)
+ *  i = BinnarySearch(arr, x)
+ *  if (i == -1)
+ *      return false
+ *  else
+ *      return true
+ */
+
+/*
+ * insert(x)
+ *  i = binnarySearchNearest(arr, x)
+ *
+ *
+ *
+ *
+ *  zdjęcia zrobiłem
+ */
+int main() {
+
+}
